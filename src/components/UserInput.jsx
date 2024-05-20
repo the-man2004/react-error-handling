d845d453964d9315777bbaa1a3897f71ed275d08
the-man2004 <heinrichhb111@gmail.com>
2024-05-20 15:59:29 -0800

    Fixed the errors in the project files.

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -9,7 +9,7 @@ export default function UserInput({ onChange, userInput }) {
             required
             value={userInput.initialInvestment}
             onChange={(event) =>
-              onChange('initialInvestment', event.target.value)
+              onChange("initialInvestment", event.target.value)
             }
           />
         </p>
@@ -20,7 +20,7 @@ export default function UserInput({ onChange, userInput }) {
             required
             value={userInput.annualInvestment}
             onChange={(event) =>
-              onChange('annualInvestment', event.target.value)
+              onChange("annualInvestment", event.target.value)
             }
           />
         </p>
@@ -32,9 +32,7 @@ export default function UserInput({ onChange, userInput }) {
             type="number"
             required
             value={userInput.expectedReturn}
-            onChange={(event) =>
-              onChange('expectedReturn', event.target.value)
-            }
+            onChange={(event) => onChange("expectedReturn", event.target.value)}
           />
         </p>
         <p>
@@ -43,7 +41,7 @@ export default function UserInput({ onChange, userInput }) {
             type="number"
             required
             value={userInput.duration}
-            onChange={(event) => onChange('duration', event.target.value)}
+            onChange={(event) => onChange("duration", event.target.value)}
           />
         </p>
       </div>
--- src/util/investment.js
+++ src/util/investment.js
@@ -4,12 +4,10 @@
 // - annualInvestment: The amount invested every year
 // - expectedReturn: The expected (annual) rate of return
 // - duration: The investment duration (time frame)
-export function calculateInvestmentResults({
-  initialInvestment,
-  annualInvestment,
-  expectedReturn,
-  duration,
-}, results) {
+export function calculateInvestmentResults(
+  { initialInvestment, annualInvestment, expectedReturn, duration },
+  results
+) {
   let investmentValue = initialInvestment;
 
   for (let i = 0; i < duration; i++) {
@@ -27,9 +25,9 @@ export function calculateInvestmentResults({
 // The browser-provided Intl API is used to prepare a formatter object
 // This object offers a "format()" method that can be used to format numbers as currency
 // Example Usage: formatter.format(1000) => yields "$1,000"
-export const formatter = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
+export const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
   minimumFractionDigits: 0,
   maximumFractionDigits: 0,
 });
